90b4d3c79e307c1a35c4521e59ab9ddd482089b6
wats5251@fredonia.edu <wats5251@fredonia.edu>
2011-09-25 22:58:54 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '9b4b125a0d0e6c3538bda7b9997d6b01797ba73a8e279df07e488bea0a009dffe618877cd9cf9bc09dee29131d55cb5dfa4902e10050d25e4b2e9d32aa262349'
